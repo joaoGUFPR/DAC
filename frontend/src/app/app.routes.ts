4c37fe3ccfb6b1194fd1dd297d15fe985837798a
Andre Oliveira <fulcrunn@yahoo.com.br>
2025-08-11 17:52:59 -0300

    Feat R1: Tela de autocadastro

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login'
+import { Login } from './pages/login/login';
+import { Autocadastro } from './pages/autocadastro/autocadastro';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: Login },
+    { path: 'autocadastro', component: Autocadastro},
 ];
