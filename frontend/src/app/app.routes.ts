7ac82801ac5dcf880baa9ccca9e98763bec1a844
joaoGUFPR <joaoguilherme1@ufpr.br>
2025-08-16 16:18:44 -0300

    Feat R4: front Tela alteracao de perfil

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Autocadastro } from './pages/cliente/autocadastro/autocadastro';
 import { TelaInicial } from './pages/cliente/tela-inicial/tela-inicial';
+import { AlterarPerfilComponent } from './pages/cliente/alteracao-de-perfil/alteracao-de-perfil';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: Login },
     { path: 'autocadastro', component: Autocadastro},
-    { path: 'tela-inicial', component: TelaInicial}
+    { path: 'tela-inicial', component: TelaInicial},
+    {path: 'alteraco-perfil', component: AlterarPerfilComponent}
 ];
